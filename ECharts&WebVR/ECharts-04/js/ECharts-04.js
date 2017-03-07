eba028804186b07f8b80384be44a3da1f905be7e
silentsora <silentsora@yahoo.co.jp>
2017-03-07 14:57:15 +0800

    170307,echarts-04

--- ECharts&WebVR/ECharts-04/js/ECharts-04.js
+++ ECharts&WebVR/ECharts-04/js/ECharts-04.js
@@ -25,6 +25,13 @@
 		for (var i = 0;i < rawData.length;i++){
 			tempData.date.push(rawData[i].Date);
 
+			// date format
+			var tempArr = [];
+			tempArr[0] = tempData.date[i].slice(0,4);
+			tempArr[1] = tempData.date[i].slice(4,6);
+			tempArr[2] = tempData.date[i].slice(6,8);
+			tempData.date[i] = tempArr.join('-');
+
 			tempData.data[i] = [];
 			tempData.data[i].push(rawData[i].Open,rawData[i].Close,rawData[i].Low,rawData[i].High);
 			
@@ -33,6 +40,19 @@
 		return tempData;
 	};
 })();
+
+function calculateMA(dayCount,data){
+	var result = [];
+	for(var i = dayCount-1;i<data.length;i++){
+		result[i] = 0;
+		for(var j = 0;j<dayCount;j++){
+			result[i] += parseFloat(data[i-j][1]);
+		}
+		result[i] /= dayCount;
+		result[i] = result[i].toFixed(2);
+	};
+	return result;
+}
 
 function buildChart(data){
 	var myChart = echarts.init(document.getElementById("main"));
@@ -48,35 +68,56 @@ function buildChart(data){
 			}
 		},
 		legend: {
-			data: ['日K']
+			data: ['日K','MA5','MA10','MA20','MA30']
 		},
-		grid: {
-			bottom: '15%'
-		},
-		xAxis: {
-			type: 'category',
-			data: data.date,
-			scale: true,
-			min: 'dataMin',
-			max: 'dataMax'
-		},
-		yAxis: {
-			scale: true,
-			splitArea: {
-				show: true
+		grid: [
+			{
+				bottom: '40%'
+			},
+			{
+				top: '70%',
+				bottom: '14%'
 			}
-		},
+		],
+		xAxis: [
+			{
+				type: 'category',
+				data: data.date,
+				scale: true,
+				min: 'dataMin',
+				max: 'dataMax'
+			},
+			{
+				type: 'category',
+				gridIndex: 1,
+				data: data.date
+			}
+		],
+		yAxis: [
+			{
+				scale: true,
+				splitArea: {
+					show: true
+				}
+			},
+			{
+				scale: true,
+				gridIndex: 1
+			}
+		],
 		dataZoom: [
 			{
 				type: 'inside',
-				start: 50,
+				xAxisIndex: [0,1],
+				start: 75,
 				end: 100
 			},
 			{
 				type: 'slider',
 				show: true,
-				y: '90%',
-				start: 50,
+				xAxisIndex: [0,1],
+				y: '92%',
+				start: 75,
 				end: 100
 			}
 		],
@@ -84,7 +125,58 @@ function buildChart(data){
 			{
 				name: '日K',
 				type: 'candlestick',
-				data: data.data,
+				data: data.data
+			},
+			{
+				name:'成交量',
+				type: 'bar',
+				xAxisIndex: 1,
+				yAxisIndex: 1,
+				data: data.volume
+			},
+			{
+				name: 'MA5',
+				type: 'line',
+				yAxisIndex: 0,
+				yAxisIndex: 0,
+				smooth: true,
+				lineStyle: {
+                    normal: {opacity: 0.3}
+                },
+				data: calculateMA(5,data.data)
+			},
+			{
+				name: 'MA10',
+				type: 'line',
+				yAxisIndex: 0,
+				yAxisIndex: 0,
+				smooth: true,
+				lineStyle: {
+                    normal: {opacity: 0.3}
+                },
+				data: calculateMA(10,data.data)
+			},
+			{
+				name: 'MA20',
+				type: 'line',
+				yAxisIndex: 0,
+				yAxisIndex: 0,
+				smooth: true,
+				lineStyle: {
+                    normal: {opacity: 0.3}
+                },
+				data: calculateMA(20,data.data)
+			},
+			{
+				name: 'MA30',
+				type: 'line',
+				yAxisIndex: 0,
+				yAxisIndex: 0,
+				smooth: true,
+				lineStyle: {
+                    normal: {opacity: 0.3}
+                },
+				data: calculateMA(30,data.data)
 			}
 		]
 	};
